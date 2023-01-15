38c05d330929493a113954f78e4f1a84b3a2f1e3
Tamara Fedyanova <112993598+toma-fedyanova@users.noreply.github.com>
2023-01-15 09:07:52 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -38,8 +38,40 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let arr = [];
+    let str = '';
+    let char = [];
+    for (let i =  0; i < expr.length; i += 10) {
+      let str = expr.slice(i, (i + 10));
+        if (str == '**********') {
+          str = ' ';
+        }
+      arr.push(str);
+    }
+    for (let i = 0; i < arr.length; i++) {
+      let text = '';
+      if (arr[i] == ' ') {
+        char.push(arr[i]);
+      }
+      else {
+        for (let j = 0; j < arr[i].length - 1; j += 2) {
+          str = arr[i].substr(j, 2);
+            if (str == '11') {
+              str = '-';
+            }
+           else if (str == '10') {
+               str = '.';
+            }
+           else {
+             str = '';
+            }
+         text +=str;
+        }
+      char.push(MORSE_TABLE[text]);
+    }
+  }
+  return char.join('');
+  }
 
 module.exports = {
     decode
